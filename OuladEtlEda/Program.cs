d5175583abc83d189ef2d92564404d17ad4b8207
Víctor M. Blanco N. <blanquitoh@hotmail.com>
2025-06-08 20:00:49 -0400

    Updating sql connection string.

--- OuladEtlEda/Program.cs
+++ OuladEtlEda/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using OuladEtlEda;
 
 var options = new DbContextOptionsBuilder<OuladContext>()
-    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Oulad;Trusted_Connection=True;")
+    .UseSqlServer("Data Source=BLANQUITOH-SERV;User ID=Blanquitoh;Password=welc0me;Database=Oulad;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False")
     .Options;
 
 using var db = new OuladContext(options);
