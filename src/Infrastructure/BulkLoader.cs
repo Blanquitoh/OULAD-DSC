8dc81321bac85e63ff591daa267dccd24fdec19f
Víctor Blanco <34665275+Blanquitoh@users.noreply.github.com>
2025-06-09 20:17:56 -0400

    Deduplicate entities before bulk insert

--- src/Infrastructure/BulkLoader.cs
+++ src/Infrastructure/BulkLoader.cs
@@ -1,5 +1,6 @@
 using EFCore.BulkExtensions;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace OuladEtlEda.Infrastructure;
 
@@ -7,6 +8,27 @@ public class BulkLoader
 {
     public async Task BulkInsertAsync<T>(DbContext context, IList<T> entities) where T : class
     {
+        var entityType = context.Model.FindEntityType(typeof(T));
+        var keyProps = entityType?.FindPrimaryKey()?.Properties;
+        if (keyProps != null && keyProps.Count > 0)
+        {
+            var seen = new HashSet<string>();
+            var deduped = new List<T>(entities.Count);
+            foreach (var entity in entities)
+            {
+                var key = string.Join('|', keyProps.Select(p => typeof(T).GetProperty(p.Name)!.GetValue(entity)?.ToString()));
+                if (seen.Add(key))
+                {
+                    deduped.Add(entity);
+                }
+                else
+                {
+                    Log.Information("Skipping duplicate {EntityType} with key {Key}", typeof(T).Name, key);
+                }
+            }
+            entities = deduped;
+        }
+
         await context.BulkInsertAsync(entities);
     }
 }
